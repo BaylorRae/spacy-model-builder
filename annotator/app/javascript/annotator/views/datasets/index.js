e6c56f3076600c2abecc07e8eea93b5798bcd47f
Baylor Rae <baylor@thecodedeli.com>
2020-06-24 10:57:53 -0500

    add dataset show view

--- annotator/app/javascript/annotator/views/datasets/index.js
+++ annotator/app/javascript/annotator/views/datasets/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Router } from '@reach/router'
 
 import List from './list'
+import Show from './show'
 
 const Datasets = () => (
   <Router>
+    <Show path="/:id" />
     <List path="/" />
   </Router>
 )
---
+++ annotator/app/javascript/annotator/views/datasets/show/index.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Link } from '@reach/router'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+
+const DATASET = gql`
+  query ($id: Int!) {
+    dataset(id: $id) {
+      id
+      title
+      
+      texts {
+        id
+        text
+      }
+    }
+  }
+`
+
+function useDataset(id) {
+  const { loading, error, data } = useQuery(
+    DATASET,
+    {
+      variables: {id: parseInt(id)}
+    }
+  )
+
+  return {
+    loading,
+    error,
+    dataset: loading || error ? null : data.dataset
+  }
+}
+
+const Texts = ({ texts }) => (
+  <table className="table">
+    <thead>
+      <tr>
+        <th>Summary</th>
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      {texts.map(text => (
+        <tr key={text.id}>
+          <td>{text.text.substr(0, 50)}...</td>
+          <td>
+            <Link to={text.id.toString()}>Annotate &raquo;</Link>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
+
+const Show = ({ id }) => {
+  const { loading, dataset } = useDataset(id)
+
+  return (
+    <>
+      <Link to="../">&laquo; All Datasets</Link>
+      {loading && <div>Loading...</div>}
+      {!loading && (
+        <>
+          <h2 className="title">{dataset.title}</h2>
+          <Texts texts={dataset.texts} />
+        </>
+      )}
+    </>
+  )
+}
+
+export default Show
