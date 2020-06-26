e1e8d2d1972cf919b4a16e8872e9efa9e9c6bb88
Baylor Rae <baylor@thecodedeli.com>
2020-06-26 08:51:58 -0500

    query texts based on annotated status

--- annotator/app/graphql/types/dataset_type.rb
+++ annotator/app/graphql/types/dataset_type.rb
@@ -1,9 +1,21 @@
 module Types
   class DatasetType < BaseObject
+    ANNOTATED_QUERIES = {
+      nil   => nil,
+      false => 'annotated_at is null',
+      true  => 'annotated_at is not null'
+    }
+
     field :id, Integer, null: false
     field :title, String, null: false
 
     field :entities, [DatasetEntityType], null: false
-    field :texts, [DatasetTextType], null: false
+
+    field :texts, [DatasetTextType], null: false do
+      argument :annotated, Boolean, required: false
+    end
+    def texts(annotated: nil)
+      object.texts.where(ANNOTATED_QUERIES[annotated])
+    end
   end
 end
